6dfd2b0e13b6e0cc46d9ad3d6096f122c1ae5901
andrewsoncha <65234283+andrewsoncha@users.noreply.github.com>
2026-06-30 17:07:26 -0400

    Added logic to mutate global_map
    Added logic to randomly flip the values of "global_map" of
    observation

--- src/trainer/model.py
+++ src/trainer/model.py
@@ -6,6 +6,7 @@ from tensorflow.keras import ops
 
 from tensorflow.keras.layers import Input, Conv2D, Dense, MaxPool2D, Flatten, Embedding, Concatenate, Softmax
 from tensorflow.keras import Model
+from tensorflow.keras.utils import plot_model
 
 from utils import Factory
 
@@ -17,6 +18,7 @@ class NNModel:
         self.observation_space = obs_space
         self.action_space = act_space
         self.model = self.create_model()
+        plot_model(self.model, to_file='model.png')
 
     @tf.function
     def __call__(self, obs):
