fa4aeb2b31e50534ccdcf5e3d540859a0d82949c
andrewsoncha <65234283+andrewsoncha@users.noreply.github.com>
2026-08-16 16:19:59 -0400

    Added fineTune.py

---
+++ fineTune.py
@@ -0,0 +1,65 @@
+import argparse
+from dataclasses import dataclass, field
+
+from src.gym import PathPlanningGymFactory
+from src.trainer.agent import AgentFactory
+from src.base.evaluator import Evaluator
+from src.base.logger import Logger
+from src.trainer.trainer import TrainerFactory
+from utils import AbstractParams
+import tensorflow as tf
+
+@dataclass
+class PathPlanningParams(AbstractParams):
+    trainer: TrainerFactory.default_param_type() = field(default_factory=TrainerFactory.default_params)
+    gym: PathPlanningGymFactory.default_param_type() = field(default_factory = PathPlanningGymFactory.default_params)
+    logger: Logger.Params = field(default_factory = Logger.Params)
+    evaluator: Evaluator.Params = field(default_factory = Evaluator.Params)
+    agent: AgentFactory.default_param_type() = field(default_factory = AgentFactory.default_params)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser = PathPlanningParams.add_args_to_parser(parser)
+    parser.add_argument('-pt', '--pretrained', default=None, help='Path to the pretrained model')
+    args = parser.parse_args()
+
+    params, args = PathPlanningParams.from_parsed_args(args)
+    log_dir = params.create_folders(args)
+
+    gym = PathPlanningGymFactory.create(params.gym)
+
+    action_space = gym.action_space
+    obs_space = gym.observation_space
+
+    agent = AgentFactory.create(params.agent, obs_space=obs_space, act_space=action_space)
+
+    if args.pretrained is None:
+        print('fineTune.py requires the path to the pretrained model!')
+        print('exiting program!')
+        exit(1)
+
+    # Added code to load pre-trained model files. -- August 16th, 2026, Andrew Chang.
+    pretrained_path = args.pretrained + '/models'
+    pretrained_model = agent.load_keras(pretrained_path)
+    print('Loaded pretrained weights from {}'.format(pretrained_path))
+
+    if args.verbose:
+        agent.summary()
+
+    logger = Logger(params.logger, log_dir, agent)
+    trainer = TrainerFactory.create(params.trainer, gym=gym, logger=logger, agent=agent)
+    evaluator = Evaluator(params.evaluator, trainer, gym)
+    logger.evaluator = evaluator
+
+    params.save_to(params.log_dir + "config.json")
+
+    if not args.gpu and args.gpu_id is None:
+        print("Running on CPU")
+    else:
+        print(f"Running on GPU {args.gpu_id}" if args.gpu_id is not None else "Running on GPU")
+
+    trainer.train()
+    agent.save_keras(params.log_dir + 'models/')
+
+    gym.close()
